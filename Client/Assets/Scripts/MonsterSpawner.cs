83f1741957832a671f814dcbe8dab59649cf762b
huijae <t4r@naver.com>
2024-11-25 02:51:50 +0900

    za

--- Client/Assets/Scripts/MonsterSpawner.cs
+++ Client/Assets/Scripts/MonsterSpawner.cs
@@ -55,50 +55,60 @@ public class MonsterSpawner : MonoBehaviour
             {
                 for (int j = 0; j < arr[i].count; ++j)
                 {
-                    float x = UnityEngine.Random.Range(-25f, 25f);
+                    float x = UnityEngine.Random.Range(-15f, 15f);
                     float y = (float)Math.Sqrt(225 - x * x) * (UnityEngine.Random.Range(0, 2) == 0 ? -1 : 1);
 
                     switch (arr[i].monsterName)
                     {
                         case MonsterName.Snake:
-                            var snake = ObjectPoolManager.Instance.Get<Snake>();
-                            var pos = snake.transform.position;
-                            pos.x = x;
-                            pos.y = y;
-                            snake.transform.position = pos;
-                            snake.Initialize();
+                            {
+                                var snake = ObjectPoolManager.Instance.Get<Snake>();
+                                var pos = snake.transform.position;
+                                pos.x = x;
+                                pos.y = y;
+                                snake.transform.position = pos;
+                                snake.Initialize();
+                            }
                             break;
                         case MonsterName.Viper:
-                            var viper = ObjectPoolManager.Instance.Get<Viper>();
-                            pos = viper.transform.position;
-                            pos.x = x;
-                            pos.y = y;
-                            viper.transform.position = pos;
-                            viper.Initialize();
+                            {
+                                var viper = ObjectPoolManager.Instance.Get<Viper>();
+                                var pos = viper.transform.position;
+                                pos.x = x;
+                                pos.y = y;
+                                viper.transform.position = pos;
+                                viper.Initialize();
+                            }
                             break;
                         case MonsterName.Golem:
-                            var golem = ObjectPoolManager.Instance.Get<Golem>();
-                            pos = golem.transform.position;
-                            pos.x = x;
-                            pos.y = y;
-                            golem.transform.position = pos;
-                            golem.Initialize();
+                            {
+                                var golem = ObjectPoolManager.Instance.Get<Golem>();
+                                var pos = golem.transform.position;
+                                pos.x = x;
+                                pos.y = y;
+                                golem.transform.position = pos;
+                                golem.Initialize();
+                            }
                             break;
                         case MonsterName.Skeleton:
-                            var skeleton = ObjectPoolManager.Instance.Get<Skeleton>();
-                            pos = skeleton.transform.position;
-                            pos.x = x;
-                            pos.y = y;
-                            skeleton.transform.position = pos;
-                            skeleton.Initialize();
+                            {
+                                var skeleton = ObjectPoolManager.Instance.Get<Skeleton>();
+                                var pos = skeleton.transform.position;
+                                pos.x = x;
+                                pos.y = y;
+                                skeleton.transform.position = pos;
+                                skeleton.Initialize();
+                            }
                             break;
                         case MonsterName.Slime:
-                            var slime = ObjectPoolManager.Instance.Get<Slime>();
-                            pos = slime.transform.position;
-                            pos.x = x;
-                            pos.y = y;
-                            slime.transform.position = pos;
-                            slime.Initialize();
+                            {
+                                var slime = ObjectPoolManager.Instance.Get<Slime>();
+                                var pos = slime.transform.position;
+                                pos.x = x;
+                                pos.y = y;
+                                slime.transform.position = pos;
+                                slime.Initialize();
+                            }
                             break;
                     }
                 }
